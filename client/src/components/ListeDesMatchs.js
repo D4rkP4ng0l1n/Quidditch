d7df05410c6811dfbc0561d97e8ae9354ebc45e2
Jules Cayrol <jules.cayrol@iut-tlse3.fr>
2023-04-18 13:47:16 +0200

    Commit création v1 des components match

---
+++ client/src/components/ListeDesMatchs.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ListeDesMatchs = () => {
+    return (
+        <article>
+            <header>
+                <h1>Liste des matchs</h1>
+            </header>
+            <div>
+                <button>Ajouter un match</button>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Date</th>
+                            <th>Heure</th>
+                            <th>Equipe adverse</th>
+                            <th>Lieu</th>
+                            <th>Score (Domicile - Adversaire)</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </article>
+    );
+};
+
+export default ListeDesMatchs;
