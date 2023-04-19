79407432a844f47b28b3cf1de4c14f08cf543bfc
Jules Cayrol <jules.cayrol@iut-tlse3.fr>
2023-04-19 10:18:50 +0200

    Ajout du css sur toutes les pages matchs

--- client/src/components/SelectFeuilleMatch.js
+++ client/src/components/SelectFeuilleMatch.js
@@ -3,27 +3,27 @@ import React from 'react';
 const SelectFeuilleMatch = () => {
     return (
         <article>
-            <header>
+            <header className='containerTitre'>
                 <h1>Sélection des joueurs de la feuille de match</h1>
             </header>
-            <div>
-                <form>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th>Photo</th>
-                                <th>Taille</th>
-                                <th>Poids</th>
-                                <th>Poste préféré</th>
-                                <th>Titulaire ou remplaçant</th>
-                                <th>Sélectionner</th>
-                            </tr>
-                        </tbody>
-                    </table>
+            <div className='containerTabAndButtons'>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Photo</th>
+                            <th>Taille</th>
+                            <th>Poids</th>
+                            <th>Poste préféré</th>
+                            <th>Titulaire ou remplaçant</th>
+                            <th>Sélectionner</th>
+                        </tr>
+                    </tbody>
+                </table>
+                <div>
                     <p>7 Joueurs Titulaires requis pour valider</p>
-                    <input type="submit" value="Valider" />
-		            <input type="button" value="Annuler" onclick=""/>
-                </form>
+                    <button>Valider</button>
+                    <button>Annuler</button>
+                </div>
             </div>
         </article>
     );
